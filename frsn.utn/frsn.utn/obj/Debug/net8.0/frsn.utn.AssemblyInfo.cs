8f6e99faed8e46ce41126ec9df65c6e46b8edde8
franco <franquito05@yahoo.com.ar>
2024-10-23 14:19:18 -0300

    ProyectoWeb, y funciones CargarDatos y MostrarProyectos

--- frsn.utn/frsn.utn/obj/Debug/net8.0/frsn.utn.AssemblyInfo.cs
+++ frsn.utn/frsn.utn/obj/Debug/net8.0/frsn.utn.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frsn.utn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8b5f3fabb53c3a422c542d206fe9bb18ea3c9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4990c0c59e1faeadc81892fb783b371293d7b71")]
 [assembly: System.Reflection.AssemblyProductAttribute("frsn.utn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frsn.utn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
